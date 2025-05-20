de9e6378d65f0bd4ea8289be0d185556f4601368
GarethNelsonKainos <gareth.nelson@kainos.com>
2025-05-20 13:25:12 +0100

    Added logs for Core

--- src/main/java/uk/nhs/hackathon/ctrlaltelite/localserver/core/Core.java
+++ src/main/java/uk/nhs/hackathon/ctrlaltelite/localserver/core/Core.java
@@ -1,20 +1,24 @@
 package uk.nhs.hackathon.ctrlaltelite.localserver.core;
 
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.ResourceAccessException;
 import uk.nhs.hackathon.ctrlaltelite.localserver.inbound.TriageRequest;
 import uk.nhs.hackathon.ctrlaltelite.localserver.inbound.TriageResponse;
 import uk.nhs.hackathon.ctrlaltelite.localserver.outbound.RestApiCentralAdapter;
+
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 
 @Service
 public class Core {
     private final RestApiCentralAdapter adapter;
     private final InternalTriageEngine triageEngine;
+    private final Logger logger;
 
     public Core(RestApiCentralAdapter adapter, InternalTriageEngine triageEngine) {
         this.adapter = adapter;
         this.triageEngine = triageEngine;
         getAllPathwaysForCaching();
+        this.logger = LogManager.getLogger(Core.class);
     }
 
     public void getAllPathwaysForCaching() {
@@ -23,9 +27,11 @@ public class Core {
 
     public TriageResponse getNextQuestion(TriageRequest request) {
         try {
+            logger.info("Getting next question from central triage engine...");
             return this.adapter.getNextQuestionFromServer(request);
         } catch (Exception E) {
-            System.out.println("[WARN] Unable to contact central server - falling back on local triage engine");
+            logger.info("Unable to get next question from central triage engine.");
+            logger.info("Use local triage question to get next question");
             return triageEngine.triage(request);
         }
     }
